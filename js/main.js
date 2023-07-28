7c7f60771810d5182ed1a756e6a2e81e116219a4
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-28 12:10:25 +0200

    js: edit

--- js/main.js
+++ js/main.js
@@ -23,42 +23,43 @@ if (result == true) {
 } else {
   alert("La parola non è polindroma");
 }
-// // # Pari e Dispari
 
-// // chiedo all'utente di scegliere pari o dispari
-// const evenOrOdd = prompt("Scegli pari o dispari");
-// console.log(evenOrOdd);
+// # Pari e Dispari
 
-// // chiedo all'utente un numero da 1 a 5
-// let number = parseInt(prompt("Scegli un numero da 1 a 5"));
-// console.log(number);
+// chiedo all'utente di scegliere pari o dispari
+const evenOrOdd = prompt("Scegli pari o dispari");
+console.log(evenOrOdd);
 
-// //creo una funzione per generare un numero random da 1 a 5 per il pc
-// function getNumberRandom(min, max, lstIncluded = true) {
-//   if (lstIncluded) max++;
-//   const numberRandom = Math.floor(Math.random() * (max - min) + min);
-//   return numberRandom;
-// }
+// chiedo all'utente un numero da 1 a 5
+let number = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log(number);
 
-// const generatedNumber = getNumberRandom(1, 5);
-// console.log(generatedNumber);
+//creo una funzione per generare un numero random da 1 a 5 per il pc
+function getNumberRandom(min, max, lastIncluded = true) {
+  if (lastIncluded) max++;
+  const numberRandom = Math.floor(Math.random() * (max - min) + min);
+  return numberRandom;
+}
 
-// // sommo i due numeri
-// const resultSum = number + generatedNumber;
-// console.log(resultSum);
+const generatedNumber = getNumberRandom(1, 5);
+console.log(generatedNumber);
 
-// // creo una funzione per stabilire se la somma è pari o dispari
-// function evenOdd(num) {
-//   let isEvenOrOdd;
-//   if (num % 2 == 0) {
-//     isEvenOrOdd = "pari";
-//   } else isEvenOrOdd = "dispari";
-//   return isEvenOrOdd;
-// }
+// sommo i due numeri
+const resultSum = number + generatedNumber;
+console.log(resultSum);
 
-// // dichiaro il vincitore
-// if (evenOrOdd == evenOdd(resultSum)) {
-//   alert("Hai vinto");
-// } else {
-//   alert("Hai perso");
-// }
+// creo una funzione per stabilire se la somma è pari o dispari
+function evenOdd(num) {
+  let isEvenOrOdd;
+  if (num % 2 == 0) {
+    isEvenOrOdd = "pari";
+  } else isEvenOrOdd = "dispari";
+  return isEvenOrOdd;
+}
+
+// dichiaro il vincitore
+if (evenOrOdd == evenOdd(resultSum)) {
+  alert("Hai vinto");
+} else {
+  alert("Hai perso");
+}
